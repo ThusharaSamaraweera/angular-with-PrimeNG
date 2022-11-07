6faaeaca8db366e0aa77e5ec678188e42586f972
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-11-07 22:14:51 +0530

    add customer details section

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -3,19 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
-  styleUrls: ['./customer-details.component.scss']
+  styleUrls: ['./customer-details.component.scss'],
 })
 export class CustomerDetailsComponent implements OnInit {
-  title: string = "JK"
-  name: string = "Jefferey Frankin"
-  email: string = "jefferey123@gmail.com"
-  contactNumber: string = "415 555 - 2671"
-  address: string = "Howard Street Oswege NY"
-  customerStatus: string = "active"
+  customerStatusOptions = [
+    { label: 'Active', value: 'active' },
+    { label: 'Inactive', value: 'inactive' },
+  ];
 
-  constructor() { }
+  title: string = 'JK';
+  name: string = 'Jefferey Frankin';
+  email: string = 'jefferey123@gmail.com';
+  contactNumber: string = '415 555 - 2671';
+  address: string = 'Howard Street Oswege NY';
+  customerStatus = this.customerStatusOptions[0].value
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
